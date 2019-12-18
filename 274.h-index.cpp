91245ce183a94330fbc2b0e1be3861103fa73fe1
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2019-12-18 14:50:26 -0500

    No New successful codes, just some new tests

---
+++ 274.h-index.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=274 lang=cpp
+ *
+ * [274] H-Index
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+    }
+};
+// @lc code=end
+
