6561437021387d09c29e959463997cce3d894084
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2019-12-18 12:59:51 -0500

    TwoSum with onepass Hash

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -12,14 +13,17 @@ using namespace std;
 class Solution {
 public:
   vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> numMap;
+
     for (int i = 0; i < nums.size(); i++) {
-      for (int j = i + 1; j < nums.size(); j++) {
-        if (nums[j] == target - nums[i]) {
-          vector<int> vect{i, j};
-          return vect;
-        }
+      int difference = target - nums[i];
+      if (numMap.find(difference) != numMap.end()) {
+        return vector<int>{numMap[difference], i};
+      } else {
+        numMap[nums[i]] = i;
       }
     }
+
     return {};
   }
 };
