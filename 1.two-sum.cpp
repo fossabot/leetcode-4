7ea141a0f1c6d7b3da7413b1d3af8e7f3c8015b2
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2019-12-18 11:53:47 -0500

    First Commit with twosum code in CPP

---
+++ 1.two-sum.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = i + 1; j < nums.size(); j++) {
+        if (nums[j] == target - nums[i]) {
+          vector<int> vect{i, j};
+          return vect;
+        }
+      }
+    }
+    return {};
+  }
+};
+// @lc code=end
