a069c36c674582a719addb65aa4800a70c001e88
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2019-12-19 17:40:00 -0500

    Problem 13 Solved, Roman to Int

--- 13.roman-to-integer.go
+++ 13.roman-to-integer.go
@@ -15,8 +15,8 @@
  *
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
  * and M.
- * 
- * 
+ *
+ *
  * Symbol       Value
  * I             1
  * V             5
@@ -25,64 +25,106 @@
  * C             100
  * D             500
  * M             1000
- * 
+ *
  * For example, two is written as II in Roman numeral, just two one's added
  * together. Twelve is written as, XII, which is simply X + II. The number
  * twenty seven is written as XXVII, which is XX + V + II.
- * 
+ *
  * Roman numerals are usually written largest to smallest from left to right.
  * However, the numeral for four is not IIII. Instead, the number four is
  * written as IV. Because the one is before the five we subtract it making
  * four. The same principle applies to the number nine, which is written as IX.
  * There are six instances where subtraction is used:
- * 
- * 
- * I can be placed before V (5) and X (10) to make 4 and 9. 
- * X can be placed before L (50) and C (100) to make 40 and 90. 
+ *
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
  * C can be placed before D (500) and M (1000) to make 400 and 900.
- * 
- * 
+ *
+ *
  * Given a roman numeral, convert it to an integer. Input is guaranteed to be
  * within the range from 1 to 3999.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: "III"
  * Output: 3
- * 
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: "IV"
  * Output: 4
- * 
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: "IX"
  * Output: 9
- * 
+ *
  * Example 4:
- * 
- * 
+ *
+ *
  * Input: "LVIII"
  * Output: 58
  * Explanation: L = 50, V= 5, III = 3.
- * 
- * 
+ *
+ *
  * Example 5:
- * 
- * 
+ *
+ *
  * Input: "MCMXCIV"
  * Output: 1994
  * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- * 
+ *
  */
 
-// @lc code=start
-func romanToInt(s string) int {
-    
-}
-// @lc code=end
+package romanToInt
 
+// @lc code=start
+
+var lookupTable = map[rune]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
+func romanToInt(s string) int {
+	runeArray := []rune(s)
+	number := lookupTable[runeArray[len(runeArray)-1]]
+	for index, curValue := len(runeArray)-2, 0; index >= 0; index-- {
+		char := runeArray[index]
+		previousChar := runeArray[index+1]
+		curValue = lookupTable[char]
+		switch char {
+		case 'I':
+			if previousChar == 'V' || previousChar == 'X' {
+				number -= curValue
+			} else {
+				number += curValue
+			}
+		case 'X':
+			if previousChar == 'L' || previousChar == 'C' {
+				number -= curValue
+			} else {
+				number += curValue
+			}
+		case 'C':
+			if previousChar == 'D' || previousChar == 'M' {
+				number -= curValue
+			} else {
+				number += curValue
+			}
+		default:
+			number += curValue
+		}
+	}
+	return number
+}
+
+// @lc code=end
