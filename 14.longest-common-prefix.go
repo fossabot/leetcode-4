82b44dad4853bb053c91292c28fc58fdb3acc69c
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2019-12-26 08:10:48 -0500

    Problem 14 not solved

---
+++ 14.longest-common-prefix.go
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=14 lang=golang
+ *
+ * [14] Longest Common Prefix
+ *
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (34.46%)
+ * Likes:    1819
+ * Dislikes: 1573
+ * Total Accepted:    599.9K
+ * Total Submissions: 1.7M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * Write a function to find the longest common prefix string amongst an array
+ * of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ *
+ *
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ *
+ * Note:
+ *
+ * All given inputs are in lowercase letters a-z.
+ *
+ */
+
+package longestCommonPrefix
+
+// @lc code=start
+func longestCommonPrefix(strs []string) string {
+	runeArray
+}
+
+// @lc code=end
