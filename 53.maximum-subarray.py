e025f80dc149ae63531915bfdd2f82d4fd5a7595
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2019-12-29 19:44:01 -0500

    Problem 53

--- 53.maximum-subarray.py
+++ 53.maximum-subarray.py
@@ -36,17 +36,24 @@
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        numSum = [[] for num in nums]
-        maxSum = nums[0]
-        for index in range(len(nums)):
-            for smallIndex in range(index):
-                localSum = numSum[index-1][smallIndex]+nums[index]
-                if localSum > maxSum:
-                    maxSum = localSum
-                numSum[index].append(localSum)
+        # numSum = [[val for val in range(i+1)] for i in range(len(nums))]
+        # maxSum = nums[0]
+        # for index in range(len(nums)):
+        #     for smallIndex in range(index):
+        #         localSum = numSum[index-1][smallIndex]+nums[index]
+        #         if localSum > maxSum:
+        #             maxSum = localSum
+        #         numSum[index][smallIndex] = localSum
 
-            if nums[index] > maxSum:
-                maxSum = nums[index]
-            numSum[index].append(nums[index])
-        return maxSum
+        #     if nums[index] > maxSum:
+        #         maxSum = nums[index]
+        #     numSum[index][index] = nums[index]
+        if max(nums) <= 0:
+            return max(nums)
+
+        currentSum = [0]*len(nums)
+        maxSum = 0
+        for i, num in enumerate(nums):
+            currentSum[i] = max(0, currentSum[i-1]+num)
+        return max(currentSum)
 # @lc code=end
