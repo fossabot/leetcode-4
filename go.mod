42e07d3a6661b246fe14c9b6b5e93ea3ad61c152
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2020-02-17 21:47:39 -0500

    Solved 684. Need to create union-find

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/rafee/leetcode
 
 go 1.13
+
+require (
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/theodesp/unionfind v0.0.0-20200112172429-2bf90fd5b8c5
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
--- leetcode_test.go
+++ leetcode_test.go
@@ -9,3 +9,18 @@ func TestLongestSubstring(t *testing.T) {
 		t.Error("Value not matched, got value ", val)
 	}
 }
+
+func TestFindRedundantConnection(t *testing.T) {
+	input1 := [][]int{{1, 2}, {1, 3}, {2, 3}}
+	expectedResult1 := []int{2, 3}
+	input2 := [][]int{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}}
+	expectedResult2 := []int{1, 4}
+	actualResult1 := findRedundantConnection(input1)
+	actualResult2 := findRedundantConnection(input2)
+	if actualResult1[0] != expectedResult1[0] && actualResult1[1] !=
+		expectedResult1[1] && actualResult2[0] != expectedResult2[0] &&
+		actualResult2[1] != expectedResult2[1] {
+		t.Error("Value not matched. Expected [2,3] Got Result", actualResult1)
+		t.Error("Value not matched. Expected [1,4] Got Result", actualResult2)
+	}
+}
