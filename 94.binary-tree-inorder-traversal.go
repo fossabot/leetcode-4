07f97f1d48afd657aa9b8a3fc78219fb6a40807c
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2020-03-04 19:00:09 -0500

    Problem 94 solved recursively

---
+++ 94.binary-tree-inorder-traversal.go
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=94 lang=golang
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (60.62%)
+ * Likes:    2510
+ * Dislikes: 105
+ * Total Accepted:    633.5K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ *
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ *
+ * Output: [1,3,2]
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	result := make([]int, 0)
+	result = append(inorderTraversal(root.Left), root.Val)
+	result = append(result, inorderTraversal(root.Right)...)
+	return result
+}
+
+// @lc code=end
