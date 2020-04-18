b78a99d7503dabf12c85ad5fcdd9c45e0443973f
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-04-18 12:13:25 -0400

    Problem 746 Solved

---
+++ 746.min-cost-climbing-stairs_test.go
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=746 lang=golang
+ *
+ * [746] Min Cost Climbing Stairs
+ *
+ * https://leetcode.com/problems/min-cost-climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (48.51%)
+ * Likes:    1689
+ * Dislikes: 386
+ * Total Accepted:    135.1K
+ * Total Submissions: 273.7K
+ * Testcase Example:  '[0,0,0,0]'
+ *
+ *
+ * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0
+ * indexed).
+ *
+ * Once you pay the cost, you can either climb one or two steps. You need to
+ * find minimum cost to reach the top of the floor, and you can either start
+ * from the step with index 0, or the step with index 1.
+ *
+ *
+ * Example 1:
+ *
+ * Input: cost = [10, 15, 20]
+ * Output: 15
+ * Explanation: Cheapest is start on cost[1], pay that cost and go to the
+ * top.
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * Output: 6
+ * Explanation: Cheapest is start on cost[0], and only step on 1s, skipping
+ * cost[3].
+ *
+ *
+ *
+ * Note:
+ *
+ * cost will have a length in the range [2, 1000].
+ * Every cost[i] will be an integer in the range [0, 999].
+ *
+ *
+ */
+
+package leetcode
+
+import "testing"
+
+func Test_minCostClimbingStairs(t *testing.T) {
+	type args struct {
+		cost []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test 1",
+			args: args{[]int{10, 15, 20}},
+			want: 15,
+		},
+		{
+			name: "Test 2",
+			args: args{[]int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minCostClimbingStairs(tt.args.cost); got != tt.want {
+				t.Errorf("minCostClimbingStairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
